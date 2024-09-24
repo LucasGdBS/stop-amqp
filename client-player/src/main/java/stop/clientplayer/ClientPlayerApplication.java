7db900d3cd4226751bdc2ed11d360f2206d66dd1
Lucas-PC <lgbs@cesar.school>
2024-09-23 22:44:31 -0300

    :tada: start java project

---
+++ client-player/src/main/java/stop/clientplayer/ClientPlayerApplication.java
@@ -0,0 +1,13 @@
+package stop.clientplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientPlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientPlayerApplication.class, args);
+    }
+
+}
