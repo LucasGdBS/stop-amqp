1adc7410e5235ff25dd4bedef09901a595239033
Lucas-PC <lgbs@cesar.school>
2024-09-24 14:41:28 -0300

    criando classe amqp_client

---
+++ server_side/amqp_client.py
@@ -0,0 +1,25 @@
+import pika
+from decouple import config
+
+
+class AmqpClient:
+    def __init__(self) -> None:
+        self.__host = config('HOST')
+        self.__port = int(config('PORT'))
+        self.__virtual_host = config('VIRTUAL_HOST')
+        self.__username = config('RABBIT_USERNAME')
+        self.__password = config('PASSWORD')
+        self.__connection = self.__create_chanel()
+
+    def __create_chanel(self):
+        connection_parameters = pika.ConnectionParameters(
+            host=self.__host,
+            port=self.__port,
+            virtual_host=self.__virtual_host,
+            credentials=pika.PlainCredentials(
+                username=self.__username,
+                password=self.__password,
+            ),
+        )
+
+        return pika.BlockingConnection(connection_parameters).channel()
