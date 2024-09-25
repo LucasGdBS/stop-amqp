b49485f07ffcff1a2b111d29f7fd4d06af7f4dd6
Francisco Antonio da Luz Filho <falf@cesar.school>
2024-09-25 13:48:18 -0300

    Client

---
+++ client-player/src/main/java/stop/clientplayer/Receiver.java
@@ -0,0 +1,12 @@
+package stop.clientplayer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+    }
+
+}
---
+++ client-player/src/main/java/stop/clientplayer/ampq_client_consumer.java
@@ -0,0 +1,60 @@
+package stop.clientplayer;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class ampq_client_consumer {
+    static final String fanoutExchangeName = "fanout-exchange";
+    static  String queueName;
+    static final String routingKey = "rota-um";
+
+
+    void set_queue_name(String nome){
+        queueName = nome;
+    }
+
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false, true, true);
+    }
+    @Bean
+    FanoutExchange exchange() {
+        return new FanoutExchange(fanoutExchangeName, false, true);
+    }
+
+
+    // Binding the queue to the exchange
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange());
+    }
+
+    // Configuring the container
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+            MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ampq_client_consumer.class, args);
+    }
+}
